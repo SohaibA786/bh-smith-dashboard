d3b825d583c0951940d3577de66a31dff3931227
Sohaib <asohaib575@gmail.com>
2023-08-10 16:12:42 +0500

    Lottie animations updated

--- src/components/mainComponents/infographics.jsx
+++ src/components/mainComponents/infographics.jsx
@@ -12,8 +12,9 @@ function Info() {
                 {/* Info 1 */}
                 <div className='info back1'>
                     {/* Image */}
-                    <div>
-                        <img className='infoImg' src={truck} alt='Truck' />
+                    <div style={{ position: "relative", width: "79px" }}>
+                        <lottie-player src="https://lottie.host/57666718-3877-44d2-bdb6-bcc2f6799d41/xuw9pOQ6uL.json" background="transparent" speed="1" style={{ position: "absolute", bottom: "-49px", width: "79px", height: "79px" }} loop autoplay></lottie-player>
+                        {/* <img className='infoImg' src={truck} alt='Truck' /> */}
                     </div>
                     {/* Text */}
                     <div className='infoText'>
@@ -24,8 +25,9 @@ function Info() {
                 {/* Info 2 */}
                 <div className='info back2'>
                     {/* Image */}
-                    <div>
-                        <img className='infoImg' src={chimney} alt='Truck' />
+                    <div style={{ position: "relative", width: "79px" }}>
+                        <lottie-player lottie-player src="https://lottie.host/0d1969a1-a5fb-4a21-a3ae-ab71a85e8b0e/wTrkqV768k.json"  background="transparent" speed="1" style={{position: "absolute", bottom: "-25px", width: "79px", height: "79px" }} loop autoplay></lottie-player>
+                        {/* <img className='infoImg' src={chimney} alt='Truck' /> */}
                     </div>
                     {/* Text */}
                     <div className='infoText'>
