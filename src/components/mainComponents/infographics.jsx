bea3460f2e65ddd49180f076cf2094a26c498ead
Sohaib <asohaib575@gmail.com>
2023-08-15 18:02:20 +0500

    Refactoring

--- src/components/mainComponents/infographics.jsx
+++ src/components/mainComponents/infographics.jsx
@@ -1,7 +1,4 @@
 import './infographics.css'
-import truck from '../../assets/truck.gif'
-import chimney from '../../assets/chimney.gif'
-import tree from '../../assets/tree-f.gif'
 
 
 function Info() {
@@ -14,7 +11,6 @@ function Info() {
                     {/* Image */}
                     <div style={{ position: "relative", width: "79px" }}>
                         <lottie-player src="https://lottie.host/57666718-3877-44d2-bdb6-bcc2f6799d41/xuw9pOQ6uL.json" background="transparent" speed="1" style={{ position: "absolute", bottom: "-49px", width: "79px", height: "79px" }} loop autoplay></lottie-player>
-                        {/* <img className='infoImg' src={truck} alt='Truck' /> */}
                     </div>
                     {/* Text */}
                     <div className='infoText'>
@@ -28,7 +24,6 @@ function Info() {
                     <div style={{ position: "relative", width: "79px" }}>
                         <lottie-player lottie-player src="https://lottie.host/0d1969a1-a5fb-4a21-a3ae-ab71a85e8b0e/wTrkqV768k.json" background="transparent" speed="1" style={{ position: "absolute", bottom: "-25px", width: "79px", height: "79px" }} loop autoplay>
                         </lottie-player>
-                        {/* <img className='infoImg' src={chimney} alt='Truck' /> */}
                     </div>
                     {/* Text */}
                     <div className='infoText'>
@@ -42,9 +37,6 @@ function Info() {
                     <div style={{ position: "relative", width: "79px" }}>
                         <lottie-player lottie-player src="https://lottie.host/8f72ad94-a448-41e7-920e-6515475cdc3a/cmWcU61wdR.json" background="transparent" speed="1" style={{ position: "absolute", bottom: "-45px", left: "0px", width: "90px", height: "90px" }} loop autoplay>
                         </lottie-player>
-                        {/* <lottie-player lottie-player src="https://lottie.host/1918b66c-b634-43e1-9704-10e2009da018/jUD1t2hMgo.json" background="transparent" speed="1" style={{ position: "absolute", bottom: "-100px", left: "-40px", width: "200px", height: "200px" }} loop autoplay>
-                        </lottie-player> */}
-                        {/* <img className='infoImg' src={tree} alt='Tree' /> */}
                     </div>
                     {/* Text */}
                     <div className='infoText'>
